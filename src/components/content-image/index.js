7ca8e9c467dfcc46f0f2f66e4c77ccf5d401256c
James Irving <james@elementknives.com.au>
2020-03-10 12:20:20 +1100

    rename content image block

--- src/components/content-image/index.js
+++ src/components/content-image/index.js
@@ -2,23 +2,17 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styled, { css } from 'styled-components';
 
-import { H1 } from '../type';
-import { Content } from '../Content';
 import { Container, Row, Col } from '../grid';
 import { space, breakpoints } from '../../styles';
 import { getImageUrl } from '../../utils';
+import { Image } from '../image';
 
 const HEIGHT = "400px";
 
-const Image = styled.img`
-  height: 100%;
-  object-fit: cover;
-`;
-
 const ContentCol = styled(Col)`
+  align-items: start;
   display: flex;
   flex-direction: column;
-  align-items: start;
   text-align: left;
 
   @media (min-width: ${breakpoints.lg}) {
@@ -51,30 +45,28 @@ const Wrapper = styled(Container)`
     `};
 `;
 
-const ContentImageBlock = ({ image, heading, body, contentPosition }) => {
+const ContentImage = ({ image, children, contentPosition }) => {
 
   // if content is right then image position is left
   const imageOffset = contentPosition === 'right' ? [0] : [0, 0, 0, 1 / 12, 1 / 12];
 
   return (
-    <Wrapper minHeight={HEIGHT}>
-      <Row>
-      <ContentCol width={{xs: 10 / 12, md: 8 / 12, lg: 4 / 12}} offset={[1/12]} py={{ xs: space.x3, lg: space.x4 }}>
-          {heading && (<H1>{heading}</H1>)}
-          {body && (<Content>{body}</Content>)}
+    <Wrapper my="2rem" minHeight={HEIGHT} contentPosition={contentPosition}>
+      <Row flexWrap={{ xs: 'wrap', lg: 'nowrap' }}>
+        <ContentCol width={{xs: 10 / 12, md: 8 / 12, lg: 4 / 12}} offset={[1/12]} py={{ xs: space.x3, lg: space.x4 }}>
+          {children}
         </ContentCol>
         <MediaCol width={{xs: 12/12, md: 6/12}} offset={imageOffset}>
-          <Image src={getImageUrl(image)} />
+          <Image src={getImageUrl(image)} objectPosition={image.objectPosition} isBackground/>
         </MediaCol>
       </Row>
     </Wrapper>
   )
-}
+};
 
-ContentImageBlock.propTypes = {
-  body: PropTypes.string,
+ContentImage.propTypes = {
+  children: PropTypes.node,
   contentPosition: PropTypes.oneOf(['left', 'right']),
-  heading: PropTypes.string,
   image: PropTypes.shape({
     alt: PropTypes.string,
     objectPosition: PropTypes.string,
@@ -84,4 +76,4 @@ ContentImageBlock.propTypes = {
 };
 
 
-export { ContentImageBlock };
+export { ContentImage };
