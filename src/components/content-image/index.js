8875de9fe03a610d6a969905134bf4ef1a7f9482
James Irving <james@elementknives.com.au>
2020-03-10 22:56:06 +1100

    clean up content image

--- src/components/content-image/index.js
+++ src/components/content-image/index.js
@@ -3,13 +3,12 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 
 import { Container, Row, Col } from '../grid';
-import { space, breakpoints } from '../../styles';
+import { space, breakpoints, colors } from '../../styles';
 import { getImageUrl } from '../../utils';
 import { Image } from '../image';
 
-const HEIGHT = "400px";
-
 const ContentCol = styled(Col)`
+  background-color: ${colors.grey50}
   align-items: start;
   display: flex;
   flex-direction: column;
@@ -26,7 +25,6 @@ const MediaCol = styled(Col)`
   position: relative;
 
   @media (max-width: ${breakpoints.lg}) {
-    height: ${HEIGHT};
     order: 1;
   }
 `;
@@ -45,19 +43,19 @@ const Wrapper = styled(Container)`
     `};
 `;
 
-const ContentImage = ({ image, children, contentPosition }) => {
+const ContentImage = ({ image, children, contentPosition, height }) => {
 
   // if content is right then image position is left
   const imageOffset = contentPosition === 'right' ? [0] : [0, 0, 0, 1 / 12, 1 / 12];
 
   return (
-    <Wrapper my="2rem" minHeight={HEIGHT} contentPosition={contentPosition}>
+    <Wrapper my={{xs: 0, md: "2rem"}} minHeight={height} contentPosition={contentPosition}>
       <Row flexWrap={{ xs: 'wrap', lg: 'nowrap' }}>
         <ContentCol width={{xs: 10 / 12, md: 8 / 12, lg: 4 / 12}} offset={[1/12]} py={{ xs: space.x3, lg: space.x4 }}>
           {children}
         </ContentCol>
-        <MediaCol width={{xs: 12/12, md: 6/12}} offset={imageOffset}>
-          <Image src={getImageUrl(image)} objectPosition={image.objectPosition} isBackground/>
+        <MediaCol mx={{xs: "2rem", md: 0}} mb={{xs: "2rem", md: 0}} width={{xs: 12/12, md: 6/12}} offset={imageOffset} height={height}>
+          <Image src={getImageUrl(image)} objectPosition={image.objectPosition} isBackground minHeight={height}/>
         </MediaCol>
       </Row>
     </Wrapper>
@@ -73,6 +71,7 @@ ContentImage.propTypes = {
     srcset: PropTypes.string,
     url: PropTypes.string.isRequired,
   }),
+  height: PropTypes.string, 
 };
 
 
