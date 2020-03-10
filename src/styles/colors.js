ee90181afcc39d99a52455add2c1f40655e380c7
James Irving <james@elementknives.com.au>
2020-03-10 22:57:32 +1100

    add grey colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,6 +1,15 @@
 const colors = {
     white: '#ffffff',
-    black: '#000000',
+    grey50: "#fafafa",
+    grey100: "#f5f5f5",
+    grey200: "#eeeeee",
+    grey300: "#e0e0e0",
+    grey400: "#bdbdbd",
+    grey500: "#9e9e9e",
+    grey600: "#757575",
+    grey700: "#616161",
+    grey800: "#424242",
+    black: '#212121',
 };
 
 export {colors};
