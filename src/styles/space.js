5e22dbd59cd42417fec849bcb311d44123ca7e65
James Irving <james@elementknives.com.au>
2020-03-10 22:57:49 +1100

    clean up space

--- src/styles/space.js
+++ src/styles/space.js
@@ -1,4 +1,4 @@
-const space = [0, '0.5rem', '1rem', '1.5rem', '2rem', '2.5rem', '3rem', '4rem'];
+const space = [0, '1rem', '2rem', '3rem', '4rem'];
 
 /**
  * Aliases for space, we need to keep the above structure
@@ -6,12 +6,12 @@ const space = [0, '0.5rem', '1rem', '1.5rem', '2rem', '2.5rem', '3rem', '4rem'];
  */
 /* eslint-disable prefer-destructuring */
 
-space.x0 = space[1]; // .5rem
-space.x1 = space[2]; // 1rem
-space.x01 = space[3]; // 1.5rem
-space.x2 = space[4]; // 2rem
-space.x02 = space[5]; // 2.5rem
-space.x3 = space[6]; // 3rem
-space.x4 = space[7]; // 4rem
+space.x0 = "0.5rem"; // .5rem
+space.x1 = space[1]; // 1rem
+space.x01 = "1.5rem"; // 1.5rem
+space.x2 = space[2]; // 2rem
+space.x02 = "2.5rem"; // 2.5rem
+space.x3 = space[3]; // 3rem
+space.x4 = space[4]; // 4rem
 
 export { space };
