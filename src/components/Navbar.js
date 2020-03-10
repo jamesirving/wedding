04c6d376b4d429ddd72893e3039c779c6ed8575f
James Irving <james@elementknives.com.au>
2020-03-10 22:56:49 +1100

    fix nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,98 +1,141 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import styled, { css } from 'styled-components';
 
-const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      active: false,
-      navBarActiveClass: '',
-    }
+import { Button } from './button';
+import { Container, Flex, Box } from './grid';
+import { breakpoints, colors, space, globalStyles } from '../styles';
+import { H3 } from './type';
+
+const Nav = styled.nav`
+  background-color: ${colors.white};
+  position: relative;
+  z-index: 30;
+
+  @media (min-width: ${breakpoints.md}) {
+    position: sticky;
+    top: 0;
+  }
+`
+
+const Wrapper = styled(Container)`
+  @media (min-width: ${breakpoints.md}) {
+    display: flex;
+  }
+`;
+
+const NavBarLogo = styled.div`
+  align-items: stretch;
+  display: flex;
+  flex-shrink: 0;
+  min-height: 3.25rem;
+  z-index: 2;
+`;
+
+const NavBarMenu = styled.div`
+  background-color: ${colors.white};
+  box-shadow: 0 8px 16px rgba(43, 37, 35, 0.1);
+  display: block;
+  height: 100vh;
+  padding-top: ${space.x0};
+  position: absolute;
+  transform: translateX(${props => props.isOpen ? '0' : '-140%'});
+  transition: all ${globalStyles.transitionSpeed} ${globalStyles.easing};
+  width: 100%;
+
+  @media (min-width: ${breakpoints.md}) {
+    box-shadow: none;
+    display: flex;
+    height: 100%;
+    justify-content: flex-end;
+    position: relative;
+    transform: translateY(0);
+  }
+`;
+
+const NavItems = styled(Box)`
+  display: block;
+
+  @media (min-width: ${breakpoints.md}) {
+    display: flex;
+  }
+`;
+
+const NavItem = styled(Link)`
+  color: ${colors.grey800};
+  cursor: pointer;
+  display: block;
+  flex-grow: 0;
+  flex-shrink: 0;
+  line-height: 1.5;
+  margin-top: ${space.x2};
+  padding: 0.5rem 0.75rem;
+  position: relative;
+
+  &:hover {
+    color: ${colors.grey500};
   }
 
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: 'is-active',
-            })
-          : this.setState({
-              navBarActiveClass: '',
-            })
-      }
-    )
+  @media (min-width: ${breakpoints.md}) {
+    align-items: center;
+    display: flex;
+    margin-top: 0;
   }
 
-  render() {
-    return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
+`;
+
+const StyledButton = styled(Button)`
+  justify-content: center;
+  margin-top: ${space.x3};
+  width: 90%;
+
+  @media (min-width: ${breakpoints.md}) {
+    margin-top: 0;
+    width: auto;
+  }
+`;
+
+const Navbar = () => {
+  const [NavIsOpen, setNavState] = React.useState(false);
+
+  const toggleHamburger = () => {
+    setNavState(!NavIsOpen);
+  };
+
+  return (
+    <Nav role="navigation" aria-label="main-navigation">
+      <Wrapper>
+        <NavBarLogo>
+          <Link to="/" className="navbar-item" title="home">
+            <H3>J&D 26 . 02 . 21</H3>
+          </Link>
+          {/* Hamburger menu */}
           <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
+            className={`navbar-burger burger ${NavIsOpen ? 'is-active' : ''}`}
+            data-target="navMenu"
+            onClick={() => toggleHamburger()}
           >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
+            <span />
+            <span />
+            <span />
           </div>
-        </div>
-      </nav>
-    )
-  }
-}
+        </NavBarLogo>
+        <NavBarMenu isOpen={NavIsOpen}>
+          <NavItems mr="1rem">
+            <NavItem to="/details">
+              Details
+            </NavItem>
+            <NavItem to="/travel&stay">
+              Travel & Stay
+            </NavItem>
+          </NavItems>
+          <Flex justifyContent='center'>
+            <StyledButton url="/rsvp" theme='dark'>rsvp</StyledButton>
+          </Flex>
+        </NavBarMenu>
+      </Wrapper>
+    </Nav>
+  );
+};
 
 export default Navbar
