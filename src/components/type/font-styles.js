4c9bbfc39edc014b6505025427a9fc14ad49c37a
jamesirving <james@inlight.com.au>
2020-03-11 15:37:50 +1100

    fix fint-styles linting issues

--- src/components/type/font-styles.js
+++ src/components/type/font-styles.js
@@ -18,7 +18,6 @@ const coreStyles = css`
 const headingStyles = css`
   ${coreStyles}
   font-family: ${globalStyles.headingFontFamily};
-  // font-weight: ${fontWeight.bold};
   letter-spacing: 1px;
   line-height: 1.2;
 `;
