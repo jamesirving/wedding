0d38cd12dedc417ca18655785bfe20fe6f4c6546
jamesirving <james@inlight.com.au>
2020-09-22 17:57:51 +1000

    feat: add reds to colours

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,15 +1,27 @@
 const colors = {
-    white: '#ffffff',
-    grey50: "#fafafa",
-    grey100: "#f5f5f5",
-    grey200: "#eeeeee",
-    grey300: "#e0e0e0",
-    grey400: "#bdbdbd",
-    grey500: "#9e9e9e",
-    grey600: "#757575",
-    grey700: "#616161",
-    grey800: "#424242",
-    black: '#212121',
+  white: '#ffffff',
+  black: '#212121',
+
+  grey50: '#fafafa',
+  grey100: '#f5f5f5',
+  grey200: '#eeeeee',
+  grey300: '#e0e0e0',
+  grey400: '#bdbdbd',
+  grey500: '#9e9e9e',
+  grey600: '#757575',
+  grey700: '#616161',
+  grey800: '#424242',
+
+  red50: '#ffebee',
+  red100: '#ffcdd2',
+  red200: '#ef9a9a',
+  red300: '#e57373',
+  red400: '#ef5350',
+  red500: '#f44336',
+  red600: '#e53935',
+  red700: '#d32f2f',
+  red800: '#c62828',
+  red900: '#b71c1c',
 };
 
-export {colors};
+export { colors };
