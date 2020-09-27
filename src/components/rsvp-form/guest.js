99bbce3d175485798c2b70a6c4049f99b0573cbb
jamesirving <james@inlight.com.au>
2020-09-27 12:13:02 +1000

    refactor: tweak layout for guest

--- src/components/rsvp-form/guest.js
+++ src/components/rsvp-form/guest.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import { Col, Row } from '../grid';
@@ -7,11 +7,14 @@ import { P } from '../typography';
 import { TextField, RadioGroup, CheckboxGroup } from '../form-fields';
 import { Button } from '../button';
 
-const StyledRow = styled(Row)`
+const GuestWrapper = styled.div`
+  border: 2px solid transparent;
+  border-radius: 20px;
   position: relative;
+  margin-bottom: 1rem;
 
-  & :hover {
-    background-color: light-grey;
+  &:hover {
+    background-color: ${colors.grey50};
   }
 `;
 
@@ -19,82 +22,86 @@ const Guest = ({ index, arrayHelpers }) => {
   const colWidth = { xs: 12 / 12, lg: 4 / 12 };
 
   return (
-    <StyledRow flexWrap="wrap" mb={1} boarderRadius="20px" backgroundColor={colors.grey50}>
-      <Col width={1} my={1}>
-        {index < 1 ? <P> Your details: </P> : <P> Guest {index + 1} details: </P>}
-      </Col>
-      <Col width={colWidth} mb={1}>
-        <TextField label="First Name" name={`guests[${index}].givenName`} fullWidth required />
-      </Col>
-      <Col width={colWidth} mb={1}>
-        <TextField label="Last Name" name={`guests[${index}].familyName`} fullWidth required />
-      </Col>
-      <Col width={colWidth} display="flex" mb={1}>
-        <TextField label="Email" name={`guests[${index}].email`} fullWidth required />
-      </Col>
-      <Col width={{ xs: 12 / 12, lg: 6 / 12 }} my={1}>
-        <RadioGroup
-          legend="RSVP"
-          ariaLabel="RSVP response"
-          name={`guests[${index}].rsvp`}
-          options={[
-            {
-              label: 'Yes, I will be attending',
-              optionValue: 'yes',
-            },
-            {
-              label: 'No, unfortunately I am unable to attend',
-              optionValue: 'no',
-            },
-          ]}
-        />
-      </Col>
-      <Col width={{ xs: 12 / 12, lg: 6 / 12 }} my={1}>
-        <CheckboxGroup
-          legend="Dietary Requirements"
-          ariaLabel="Dietary Requirements"
-          name={`guests[${index}].dietaryRequirements`}
-          options={[
-            {
-              label: 'Vegan',
-              optionName: `guests[${index}].dietaryRequirements.vegan`,
-            },
-            {
-              label: 'Vegetarian',
-              optionName: `guests[${index}].dietaryRequirements.vegetarian`,
-            },
-            {
-              label: 'Nut allergy',
-              optionName: `guests[${index}].dietaryRequirements.nut`,
-            },
-            {
-              label: 'Gluten intolerance',
-              optionName: `guests[${index}].dietaryRequirements.gluten`,
-            },
-            {
-              label: 'No dietary requirements',
-              optionName: `guests[${index}].dietaryRequirements.none`,
-            },
-          ]}
-        />
-      </Col>
+    <GuestWrapper>
+      <Row my={1}>
+        <Col width={1}>{index < 1 ? <P> Your details: </P> : <P> Guest {index + 1} details: </P>}</Col>
+      </Row>
+      <Row flexWrap="wrap" mb={1}>
+        <Col width={colWidth} mb={1}>
+          <TextField label="First Name" name={`guests[${index}].givenName`} fullWidth required />
+        </Col>
+        <Col width={colWidth} mb={1}>
+          <TextField label="Last Name" name={`guests[${index}].familyName`} fullWidth required />
+        </Col>
+        <Col width={colWidth} display="flex" mb={1}>
+          <TextField label="Email" name={`guests[${index}].email`} fullWidth required />
+        </Col>
+      </Row>
+      <Row flexWrap="wrap" my={1}>
+        <Col width={{ xs: 12 / 12, lg: 6 / 12 }} mb={1}>
+          <RadioGroup
+            legend="RSVP"
+            ariaLabel="RSVP response"
+            name={`guests[${index}].rsvp`}
+            options={[
+              {
+                label: 'Yes, I will be attending',
+                optionValue: 'yes',
+              },
+              {
+                label: 'No, unfortunately I am unable to attend',
+                optionValue: 'no',
+              },
+            ]}
+          />
+        </Col>
+        <Col width={{ xs: 12 / 12, lg: 6 / 12 }}>
+          <CheckboxGroup
+            legend="Dietary Requirements"
+            ariaLabel="Dietary Requirements"
+            name={`guests[${index}].dietaryRequirements`}
+            options={[
+              {
+                label: 'Vegan',
+                optionName: `guests[${index}].dietaryRequirements.vegan`,
+              },
+              {
+                label: 'Vegetarian',
+                optionName: `guests[${index}].dietaryRequirements.vegetarian`,
+              },
+              {
+                label: 'Nut allergy',
+                optionName: `guests[${index}].dietaryRequirements.nut`,
+              },
+              {
+                label: 'Gluten intolerance',
+                optionName: `guests[${index}].dietaryRequirements.gluten`,
+              },
+              {
+                label: 'No dietary requirements',
+                optionName: `guests[${index}].dietaryRequirements.none`,
+              },
+            ]}
+          />
+        </Col>
 
-      {index > 0 && (
-        <Button
-          p={1}
-          top="0"
-          right="0"
-          variant="close"
-          position="absolute"
-          type="button"
-          onClick={() => {
-            arrayHelpers.remove(index); // remove a guest from the list
-          }}
-        >
-          X
-        </Button>
-      )}
-    </StyledRow>
+        {index > 0 && (
+          <Button
+            p={1}
+            top="0"
+            right="0"
+            variant="close"
+            position="absolute"
+            type="button"
+            onClick={() => {
+              arrayHelpers.remove(index); // remove a guest from the list
+            }}
+          >
+            X
+          </Button>
+        )}
+      </Row>
+    </GuestWrapper>
   );
 };
 
