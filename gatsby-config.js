e255fc27d457aca0c899f0abf2daa0b3a447cb5e
jamesirving <james@inlight.com.au>
2020-09-27 17:26:21 +1000

    fix: clean up config and readme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'James & Dina 26.02.21',
     description:
       'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
   },
@@ -74,4 +74,4 @@ module.exports = {
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
-}
+};
--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,14 +1,9 @@
 import CMS from 'netlify-cms-app';
-import uploadcare from 'netlify-cms-media-library-uploadcare';
-import cloudinary from 'netlify-cms-media-library-cloudinary';
 
 import IndexPagePreview from './preview-templates/index-page-preview';
 import DetailsPagePreview from './preview-templates/details-page-preview';
 import RsvpPagePreview from './preview-templates/rsvp-page-preview';
 import TravelAndStayPagePreview from './preview-templates/travel-and-stay-preview';
-
-CMS.registerMediaLibrary(uploadcare);
-CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview);
 CMS.registerPreviewTemplate('details', DetailsPagePreview);
