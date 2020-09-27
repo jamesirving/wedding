db82d32b89661a127eeac3fa19becd0ffcfd87b4
jamesirving <james@inlight.com.au>
2020-09-27 22:54:44 +1000

    fix: cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'James & Dina 26.02.21',
-    description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    description: "James and Dina's Wedding details and RSVP.",
   },
   plugins: [
     'gatsby-plugin-react-helmet',
