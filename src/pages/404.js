ba5c1a9bedeb0713216b7d7ee4ca95d17dbf668d
jamesirving <james@inlight.com.au>
2020-09-27 13:40:07 +1000

    feat: update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Layout } from '../components/layout';
 
+import { Heading, P } from '../components/typography';
+import { Feature } from '../components/feature';
+
 const NotFoundPage = () => (
   <Layout>
-    <div>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </div>
+    <Feature image={{ src: '/img/compass.jpg' }} objectPosition="0 40%" height="50vh">
+      <Heading variant="h1" color="white">
+        404 - NOT FOUND
+      </Heading>
+      <P color="white">You just hit a route that doesn&#39;t exist... the sadness.</P>
+    </Feature>
   </Layout>
 );
 
