46e0d94cbf225c5db50636375e7b4562a72ee712
jamesirving <james@inlight.com.au>
2020-09-27 16:22:42 +1000

    feat: add layout to details page preview template

--- src/cms/preview-templates/DetailsPagePreview.js
+++ src/cms/preview-templates/DetailsPagePreview.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { DetailsPageTemplate } from '../../templates/details-page';
+import { Layout } from '../../components/layout';
 
 // eslint-disable-next-line no-unused-vars
 const DetailsPagePreview = ({ entry, getAsset }) => {
   const data = entry.getIn(['data']).toJS();
 
   if (data) {
-    return <DetailsPageTemplate pageHeaderBlock={data.pageHeaderBlock || {}} detailsBlock={data.detailsBlock || {}} />;
+    return (
+      <Layout>
+        <DetailsPageTemplate pageHeaderBlock={data.pageHeaderBlock || {}} detailsBlock={data.detailsBlock || {}} />
+      </Layout>
+    );
   }
   return <div>Loading...</div>;
 };
--- src/components/base-styles/index.js
+++ src/components/base-styles/index.js
@@ -1,7 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 import { normalize } from 'styled-normalize';
 
-import { headingStyles, headingVariants, pStyles, bodyVariants } from '../typography';
+import { headingStyles, headingVariants, bodyVariants } from '../typography';
 
 const BaseStyles = createGlobalStyle`
   ${normalize}
