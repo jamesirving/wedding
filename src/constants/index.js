185fc8bea71105f5375285e6adc3965c5bb66850
jamesirving <james@inlight.com.au>
2020-09-28 11:48:22 +1000

    fix: typo

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 const dietaryRequirements = [
   { name: 'gluten', label: 'Gluten intolerance' },
-  { name: 'halal', label: 'Halal' },
+  { name: 'hallal', label: 'Hallal' },
   { name: 'nut', label: 'Nut allergy' },
   { name: 'vegan', label: 'Vegan' },
   { name: 'vegetarian', label: 'Vegetarian' },
