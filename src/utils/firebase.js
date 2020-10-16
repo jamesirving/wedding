27c3dc24e13e9b1bcb3e7895794772622c0b8ebd
jamesirving <james@inlight.com.au>
2020-10-16 13:05:40 +1100

    fix: update firebase to avoid ss compilation error

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,15 +2,44 @@ import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const firebaseApp = firebase.initializeApp({
+const config = {
   apiKey: 'AIzaSyBEFgIdqwWowK1PGAqatipLl_1iAPVf9po',
   authDomain: 'wedding-4fa01.firebaseapp.com',
   databaseURL: 'https://wedding-4fa01.firebaseio.com',
   projectId: 'wedding-4fa01',
   storageBucket: 'wedding-4fa01.appspot.com',
-});
+};
 
-const database = firebaseApp.firestore().collection('Wedding');
-const auth = firebase.auth();
+let firebaseApp;
 
-export { auth, database };
+const getFirebase = () => {
+  if (typeof window !== 'undefined') {
+    if (firebaseApp) return firebaseApp;
+    firebaseApp = firebase.initializeApp(config);
+    return firebaseApp;
+  }
+
+  return null;
+};
+
+const getFirebaseDB = () => {
+  const fb = getFirebase();
+
+  if (!fb) {
+    return null;
+  }
+
+  return fb.firestore().collection('Wedding');
+};
+
+const getFirebaseAuth = () => {
+  const fb = getFirebase();
+
+  if (!fb) {
+    return null;
+  }
+
+  return fb.auth();
+};
+
+export { getFirebaseAuth, getFirebaseDB };
